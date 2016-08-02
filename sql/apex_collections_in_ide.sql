1a322133501df92aa0f12b7aef662937814cff30
Mike-Gough <mrg14r@gmail.com>
2016-08-02 14:23:08 +1000

    Create apex_collections_in_ide.sql

---
+++ sql/apex_collections_in_ide.sql
@@ -0,0 +1,26 @@
+/* 
+To query data from the apex_collections view from a PL/SQL IDE you can run a 
+short anonymous PL/SQL block and define the workspace, application and session.
+*/
+
+declare
+  l_workspace_id apex_workspaces.workspace_id%type;
+  
+begin
+  select workspace_id
+    into l_workspace_id
+    from apex_workspaces
+   where workspace = '&WORKSPACE_NAME';
+   
+   -- Set workspace id
+   apex_util.set_security_group_id (l_workspace_id);
+
+   -- Set application id
+   apex_application.g_flow_id  := &APP_ID;     
+   
+   -- Set session id
+   apex_application.g_instance := &APP_SESSION;  
+end;
+
+select c.*
+  from apex_collections c;
